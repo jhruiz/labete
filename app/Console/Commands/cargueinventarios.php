03ba87fbe3cd91392bf65d252096ea411d72e3bc
root <root@vps.miggo.com.co>
2024-10-05 00:01:33 -0500

    se cambia el nombre del command para cargue de invenarios porque está en uso para el modelo

--- app/Console/Commands/cargueinventarios.php
+++ app/Console/Commands/cargueinventarios.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Cargueinventario;
 
-class cargueinventario extends Command
+class cargueinventarios extends Command
 {
     /**
      * The name and signature of the console command.
